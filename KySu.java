444f94ade209eb40f747dc6cd88db731e090bd95
luong-van-nhuan <luongnhuan906@gmail.com>
2024-02-25 14:53:59 +0700

    bai 6.1

--- KySu.java
+++ KySu.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class KySu extends Person {
 	private String nganh;
 	private int namTN;
-	private int n;
 	public KySu(String hoTen, String ngaySinh, String queQuan, String nganh, int namTN, int n) {
 		super(hoTen, ngaySinh, queQuan);
 		this.nganh = nganh;
 		this.namTN = namTN;
-		this.n = n;
 	}
-	
+
 	public KySu() {
 	}
+
 	public KySu(String hoTen, String ngaySinh, String queQuan) {
 		super(hoTen, ngaySinh, queQuan);
 	}
+
 	public String getNganh() {
 		return nganh;
 	}
@@ -34,34 +34,18 @@ public class KySu extends Person {
 		this.namTN = namTN;
 	}
 
-	public int getN() {
-		return n;
-	}
-
-	public void setN(int n) {
-		this.n = n;
-	}
 	public void nhap() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Nhập số lượng kỹ sư: ");
-		n = sc.nextInt();
-		sc.nextLine();
-		for(int i = 0; i < n; i++) {
-			super.nhap();
+		super.nhap();
 		System.out.println("Nhập ngành: ");
 		nganh = sc.nextLine();
 		System.out.println("Nhập năm tốt nghiệp: ");
 		namTN = sc.nextInt();
 		sc.nextLine();
-		}
 	}
 	public void xuat() {
-		System.out.println("Thông tin các kỹ sư");
-		for (int i = 0; i < n; i++) {
-			System.out.println("Thông tin kỹ sư " + (i +1));
 			super.xuat();
-		System.out.println("Ngành: " + nganh);
-		System.out.println("Năm tốt nghiệp: " + namTN);
-		}
-	}	
+			System.out.println("Ngành: " + nganh);
+			System.out.println("Năm tốt nghiệp: " + namTN);
+	}
 }
--- RunMain.java
+++ RunMain.java
@@ -1,9 +1,23 @@
 package bai6_1;
 
+import java.lang.constant.DirectMethodHandleDesc.Kind;
+import java.util.Scanner;
+
 public class RunMain {
+	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		KySu kySu = new KySu();
-		kySu.nhap();
-		kySu.xuat();
+		System.out.println("Nhập số lượng kỹ sư");
+		int n = sc.nextInt();
+		KySu[] dskySu  = new KySu[n];
+		for (int i = 0; i < n; i++) {
+			System.out.println("Nhập thông tin kỹ sư thứ " + (i+1));
+			dskySu[i] = new KySu();
+			dskySu[i].nhap();
+		}
+		System.out.println("Danh sách các kỹ sư");
+		for (int i = 0; i < n; i++) {
+		System.out.println("Thông tin các kỹ sư thứ " + (i + 1));
+		dskySu[i].xuat();
+		}
 	}
 }
